94bea713de4fe72372a4b62cd4601d419a6b2c57
Fabian <fabianbarraez@gmail.com>
2022-07-19 08:16:49 -0500

    printing only two bytes

--- hex_parser.py
+++ hex_parser.py
@@ -1,4 +1,4 @@
-import ctypes
+# import ctypes
 from unittest import result
 
 MAX_ARRAY_SIZE = 43
@@ -17,70 +17,41 @@ def main(filename):
     '''
     # parse all data from each array
     byte_list = []
-    n = 0
-    current_addr = 0
-    LIMIT = 4
-    past_addr = 0
-    next_addr = 0
-    for index, arr in enumerate(range(0, LIMIT)):
-        if len(read[arr]) < 19:
-            continue
-        # save all data related in each hex str
-        num_bytes    = read[arr][1:3]
-        hex_addr     = int(read[arr][3:7], 16)
-        record_type  = read[arr][7:9]
-        checksum     = read[arr][-2:]
-        _bytes       = read[arr][9:-2]
-
-        print(f"arr: {read[arr]} - len: {len(read[arr])}")
-
-        if len(read[arr]) == MAX_ARRAY_SIZE:
-            while n < len(_bytes):                 
-                byte_to_save = '0x' + str(_bytes[n:n+2])
-                byte_list.append(byte_to_save)
-                n += 2
-
-            past_addr = hex_addr
-            n = 0
-
-        elif len(read[arr]) < MAX_ARRAY_SIZE:
-
-            
-            while n < len(_bytes):                 
-                byte_to_save = '0x' + str(_bytes[n:n+2])
-                byte_list.append(byte_to_save)
-                n += 2
-                current_addr += 1
-
-            past_addr = hex_addr
-            n = 0
-
-            next_addr =  int(read[arr+1][3:7], 16)
-            diff_between_addr = int((next_addr - hex_addr) / 2) # calculates the items (0xff) I need to add 
-            print(f"next_addr {next_addr} - hex_addr {hex_addr} - diff {diff_between_addr}")
-            
-            for ff in range(0, diff_between_addr):
-                byte_to_save = '0xff'
-                byte_list.append(byte_to_save)
-
-
-
-    # add each element as hex int inside another array
-    result_list = []
-    for item in byte_list:
-        an_integer = int(item, 16)
-        result_list.append(an_integer)
     
-    print(len(result_list))
-    seq = ctypes.c_int * len(result_list)
-    arr = seq(*result_list)
-    print("PIC8 HEX: { ", end='')
-    for index, x in enumerate(arr):
-        # if last element, print a new line
-        if index == len(arr)- 1:
-            print(hex(x), end=" };\n")
-        else:
-            print(hex(x), end=', ')
+    for arr in read:
+        num_bytes            = arr[1:3]
+        current_addr_hex     = arr[3:7]
+        current_addr         = int(arr[3:7], 16)
+        record_type          = arr[7:9]
+        checksum             = arr[-2:]
+        _bytes               = arr[9:-2]
+        # print(f"num_bytes = {num_bytes}  - hex_addr = {hex_addr} - record_type = {record_type} - checksum = {checksum} - _bytes = {_bytes}")
+        # print(f"{_bytes} - {current_addr_hex}")
+        i = 0
+        while i < len(arr):
+            if arr[i] == ':':         # if the byte is a colon
+                continue        # skip it
+            # byte_list.append(byte)        # add the byte to the list
+            # packet every 2 characters
+            byte_list.append(arr[n:n+2])    # add the byte to the list
+            n += 2
+    print(byte_list)
+    # # add each element as hex int inside another array
+    # result_list = []
+    # for item in byte_list:
+    #     an_integer = int(item, 16)
+    #     result_list.append(an_integer)
+    
+    # print(len(result_list))
+    # seq = ctypes.c_int * len(result_list)
+    # arr = seq(*result_list)
+    # print("PIC8 HEX: { ", end='')
+    # for index, x in enumerate(arr):
+    #     # if last element, print a new line
+    #     if index == len(arr)- 1:
+    #         print(hex(x), end=" };\n")
+    #     else:
+    #         print(hex(x), end=', ')
 
 if __name__ == '__main__':
     main("HEV_18F13K50_v8.X.production.hex")
